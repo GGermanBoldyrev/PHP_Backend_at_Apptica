1831fa5fc2a7e4985bcc3cc25cc61d645e914d59
German Boldyrev <boldyrev_german@mail.ru>
2025-02-03 21:51:59 +0300

    initial commit + feat: migrations

---
+++ database/migrations/2025_02_03_184107_create_app_top_category_positions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('app_top_category_positions', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->integer('category_id')->index();
+            $table->integer('position')->index();
+            $table->timestamps();
+
+            $table->unique(['date', 'category_id'], 'date_category_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('app_top_category_positions');
+    }
+};
